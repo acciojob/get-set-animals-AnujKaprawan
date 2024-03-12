b97c42c8ee6f31b20a85c7c1287463c16d054c01
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2024-03-12 20:21:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -29,11 +29,3 @@ window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
 
-// Example usage
-const myCat = new Cat("Siamese");
-myCat.makeSound(); // Output: The Siamese makes a sound
-myCat.purr(); // Output: purr
-
-const myDog = new Dog("Golden Retriever");
-myDog.makeSound(); // Output: The Golden Retriever makes a sound
-myDog.bark(); // Output: woof
